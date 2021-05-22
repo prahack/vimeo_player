8c1e38e544361df8545abfdbb0dd3f728f7a097c
Prabhanu <prabhanu96@gmail.com>
2021-05-22 22:36:03 +0530

    init

---
+++ lib/vimeo_player.dart
@@ -0,0 +1,14 @@
+library vimeo_player;
+
+import 'package:flutter/cupertino.dart';
+
+class VimeoPlayer extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
---
+++ test/vimeo_player_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:vimeo_player/vimeo_player.dart';
+
+void main() {
+  test('adds one to input values', () {
+  });
+}

f35cdc3e0c11b31b3c3db419c5834cc2275e579e
Prabhanu <prabhanu96@gmail.com>
2021-06-06 18:07:11 +0530

    initial player

--- lib/vimeo_player.dart
+++ lib/vimeo_player.dart
@@ -1,14 +1,51 @@
 library vimeo_player;
 
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class VimeoPlayer extends StatelessWidget {
+  final String videoId;
 
+  const VimeoPlayer({
+    Key key,
+    this.videoId,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return WebView(
+      initialUrl: _videoPage(this.videoId),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
   }
 
+  String _videoPage(String videoId) {
+    final html = '''
+            <html>
+              <head>
+                <style>
+                  body {
+                   background-color: lightgray;
+                   margin: 0px;
+                   }
+                </style>
+                <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0">
+                <meta http-equiv="Content-Security-Policy" 
+                content="default-src * gap:; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src *; 
+                img-src * data: blob: android-webview-video-poster:; style-src * 'unsafe-inline';">
+             </head>
+             <body>
+                <iframe 
+                src="https://player.vimeo.com/video/${this.videoId}?loop=0&autoplay=0" 
+                width="100%" height="100%" frameborder="0" allow="fullscreen" 
+                allowfullscreen></iframe>
+             </body>
+            </html>
+            ''';
+    final String contentBase64 =
+        base64Encode(const Utf8Encoder().convert(html));
+    return 'data:text/html;base64,$contentBase64';
+  }
 }
